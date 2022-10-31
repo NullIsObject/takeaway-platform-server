991cefd292f3e8bfe481803a80680d9612fac55a
SNIVY <870667674@qq.com>
2022-10-31 22:02:13 +0800

    暂存

--- src/controller/user.ts
+++ src/controller/user.ts
@@ -1,129 +1,129 @@
 import { Request, Response, NextFunction } from "express";
 import jsonwebtoken from "jsonwebtoken";
-import { addUser, userSelect, userChangePassword, selectUserWallet, selectUserDiscounts } from "@/model/index";
+// import { addUser, userSelect, userChangePassword, selectUserWallet, selectUserDiscounts } from "@/model/index";
 import { token } from "@/config";
 
 const { JWTsecretKey, algorithm, tokenExpiresIn } = token;
 
 type Middleware = (req: Request, res: Response, next: NextFunction) => void;
 
-export const register: Middleware = (req, res, next) => {
-	// 用户注册
-	// 判断用户是否存在，不存在再创建账户
-	const { body } = req;
-	const userNameRule = /^[0-9a-z]{1,18}$/i;
-	const passWordRule = /^.{6,18}$/;
-	const { userName, password } = body;
-	if (!userName || !password || !userNameRule.test(userName) || !passWordRule.test(password)) {
-		res.status(403).json({
-			status: 403,
-			msg: "用户名或密码不符合规范"
-		})
-		return;
-	}
-	userSelect({ userName, password }, (err, results, fields) => {
-		if (err) {
-			res.status(500).json({
-				status: 500,
-				msg: "服务器内部错误，无法完成请求"
-			})
-			return;
-		}
-		if (results.length !== 0) {
-			res.status(403).json({
-				status: 403,
-				msg: "用户已存在"
-			})
-			return;
-		}
-		addUser({
-			userName: userName,
-			password: password
-		}, (err, results, fields) => {
-			if (err) {
-				res.status(500).json({
-					status: 500,
-					msg: "服务器内部错误，无法完成请求"
-				})
-			} else {
-				res.status(200).json({
-					status: 200,
-					msg: "用户创建成功"
-				})
-			}
-		});
-		return;
-	})
-	return;
-}
+// export const register: Middleware = (req, res, next) => {
+// 	// 用户注册
+// 	// 判断用户是否存在，不存在再创建账户
+// 	const { body } = req;
+// 	const userNameRule = /^[0-9a-z]{1,18}$/i;
+// 	const passWordRule = /^.{6,18}$/;
+// 	const { userName, password } = body;
+// 	if (!userName || !password || !userNameRule.test(userName) || !passWordRule.test(password)) {
+// 		res.status(403).json({
+// 			status: 403,
+// 			msg: "用户名或密码不符合规范"
+// 		})
+// 		return;
+// 	}
+// 	userSelect({ userName, password }, (err, results, fields) => {
+// 		if (err) {
+// 			res.status(500).json({
+// 				status: 500,
+// 				msg: "服务器内部错误，无法完成请求"
+// 			})
+// 			return;
+// 		}
+// 		if (results.length !== 0) {
+// 			res.status(403).json({
+// 				status: 403,
+// 				msg: "用户已存在"
+// 			})
+// 			return;
+// 		}
+// 		addUser({
+// 			userName: userName,
+// 			password: password
+// 		}, (err, results, fields) => {
+// 			if (err) {
+// 				res.status(500).json({
+// 					status: 500,
+// 					msg: "服务器内部错误，无法完成请求"
+// 				})
+// 			} else {
+// 				res.status(200).json({
+// 					status: 200,
+// 					msg: "用户创建成功"
+// 				})
+// 			}
+// 		});
+// 		return;
+// 	})
+// 	return;
+// }
 
-export const login: Middleware = (req, res, next) => {
-	// 用户登录
-	const { body } = req;
-	const { userName, password } = body;
-	if (!userName || !password) {
-		res.status(400).json({
-			status: 400,
-			msg: "请求缺少所需参数"
-		})
-		return;
-	}
-	userSelect({ userName, password }, (err, results, fields) => {
-		if (err) {
-			res.status(500).json({
-				status: 500,
-				msg: "未知错误，请重试"
-			});
-		} else if (results.length === 0) {//判断用户是否存在
-			res.status(403).json({
-				status: 403,
-				msg: "用户名或密码错误"
-			});
-		} else {
-			const token = jsonwebtoken.sign({ userName }, JWTsecretKey, { algorithm, expiresIn: tokenExpiresIn });
-			res.status(200).cookie("token", token, { maxAge: tokenExpiresIn }).json({
-				status: 200,
-				msg: "登录成功"
-			});
-		}
-		return;
-	});
-}
+// export const login: Middleware = (req, res, next) => {
+// 	// 用户登录
+// 	const { body } = req;
+// 	const { userName, password } = body;
+// 	if (!userName || !password) {
+// 		res.status(400).json({
+// 			status: 400,
+// 			msg: "请求缺少所需参数"
+// 		})
+// 		return;
+// 	}
+// 	userSelect({ userName, password }, (err, results, fields) => {
+// 		if (err) {
+// 			res.status(500).json({
+// 				status: 500,
+// 				msg: "未知错误，请重试"
+// 			});
+// 		} else if (results.length === 0) {//判断用户是否存在
+// 			res.status(403).json({
+// 				status: 403,
+// 				msg: "用户名或密码错误"
+// 			});
+// 		} else {
+// 			const token = jsonwebtoken.sign({ userName }, JWTsecretKey, { algorithm, expiresIn: tokenExpiresIn });
+// 			res.status(200).cookie("token", token, { maxAge: tokenExpiresIn }).json({
+// 				status: 200,
+// 				msg: "登录成功"
+// 			});
+// 		}
+// 		return;
+// 	});
+// }
 
-export const changePassword: Middleware = (req, res, next) => {
-	// 修改密码
-	const { body } = req;
-	const { userName, password, newPassword } = body;
-	if (!userName || !password || !newPassword) {
-		res.status(400).json({
-			status: 400,
-			msg: "请求缺少所需参数"
-		})
-		return;
-	}
-	userChangePassword({ userName, password, newPassword }, (isSuccess, msg) => {
-		if (isSuccess) {
-			res.status(200).json({
-				status: 200,
-				msg: "成功"
-			})
-		} else {
-			res.status(500).json({
-				status: 500,
-				msg: "服务器内部错误，无法完成请求"
-			})
-		}
-	})
-}
+// export const changePassword: Middleware = (req, res, next) => {
+// 	// 修改密码
+// 	const { body } = req;
+// 	const { userName, password, newPassword } = body;
+// 	if (!userName || !password || !newPassword) {
+// 		res.status(400).json({
+// 			status: 400,
+// 			msg: "请求缺少所需参数"
+// 		})
+// 		return;
+// 	}
+// 	userChangePassword({ userName, password, newPassword }, (isSuccess, msg) => {
+// 		if (isSuccess) {
+// 			res.status(200).json({
+// 				status: 200,
+// 				msg: "成功"
+// 			})
+// 		} else {
+// 			res.status(500).json({
+// 				status: 500,
+// 				msg: "服务器内部错误，无法完成请求"
+// 			})
+// 		}
+// 	})
+// }
 
-export const getWallet: Middleware = (req, res, next) => {
-	// 获取用户钱包信息
-	const userToken: any = jsonwebtoken.decode(req.cookies.token);
-	const userName: string = userToken?.userName;
-	// 查询钱包信息，返回所有信息，
-	// 根据钱包信息的优惠券id获取优惠券信息返回到客户端
-	// 注意代码异步执行
+// export const getWallet: Middleware = (req, res, next) => {
+// 	// 获取用户钱包信息
+// 	const userToken: any = jsonwebtoken.decode(req.cookies.token);
+// 	const userName: string = userToken?.userName;
+// 	// 查询钱包信息，返回所有信息，
+// 	// 根据钱包信息的优惠券id获取优惠券信息返回到客户端
+// 	// 注意代码异步执行
 
 
 	
-}
+// }
--- src/model/index.ts
+++ src/model/index.ts
@@ -12,69 +12,72 @@ type UserNamePassword = {
 
 const pool = mysql.createPool(mysqlConfig);
 
-export const query = (sql: QueryOptions | string, arr: any[], callBack: queryCallback) => {
-	pool.query(sql, arr, (err, results, fields) => {
-		callBack(err, results, fields)
+export const query = (sql: QueryOptions | string, arr: any[]) => {
+	return new Promise((resolve, reject) => {
+		pool.query(sql, arr, (err, results, fields) => {
+			if (err) reject();
+			resolve(results);
+		})
 	})
 };
 
-export const addUser = ({ userName, password }: UserNamePassword, callBack: SqlCallBack) => {
-	// 添加用户
-	const id = createId(),
-		createTime = Date.now();
-	const sql =
-		`INSERT INTO user_info(id,user_name,password,create_time,update_time)
-			VALUES( ? , ? , ? , ? , ? )`;
-	password = hashPassword({ userName, password });
-	query(sql, [id, userName, password, createTime, createTime], callBack)
-}
-export const userSelect = ({ userName, password }: UserNamePassword, callBack: queryCallback) => {
-	// 查询用户是否存在，根据queryCallback的results判断
-	const sql = `SELECT user_name,password FROM user_info
-	WHERE user_name = ? AND password= ?`;
-	password = hashPassword({ userName, password });
-	query(sql, [userName, password], callBack);
-}
+// export const addUser = ({ userName, password }: UserNamePassword, callBack: SqlCallBack) => {
+// 	// 添加用户
+// 	const id = createId(),
+// 		createTime = Date.now();
+// 	const sql =
+// 		`INSERT INTO user_info(id,user_name,password,create_time,update_time)
+// 			VALUES( ? , ? , ? , ? , ? )`;
+// 	password = hashPassword({ userName, password });
+// 	query(sql, [id, userName, password, createTime, createTime], callBack)
+// }
+// export const userSelect = ({ userName, password }: UserNamePassword, callBack: queryCallback) => {
+// 	// 查询用户是否存在，根据queryCallback的results判断
+// 	const sql = `SELECT user_name,password FROM user_info
+// 	WHERE user_name = ? AND password= ?`;
+// 	password = hashPassword({ userName, password });
+// 	query(sql, [userName, password], callBack);
+// }
 
-export const userChangePassword = ({ userName, password, newPassword }: UserNamePassword & { newPassword: string }, callBack: (isSuccess: boolean, msg: string) => any) => {
-	//修改密码，根据isSuccess参数判断是否成功
-	//!!!不要返回msg参数到客户端，该参数仅用于调试
-	userSelect({ userName, password }, (err, results, fields) => {
-		if (err) {
-			callBack(false, "数据库出错");
-			return;
-		}
-		if (results.length === 0) {
-			callBack(false, "用户不存在");
-			return;
-		}
-		const password = (hashPassword({ userName, password: newPassword }));
-		const sql = `UPDATE user_info
-		SET password = ?
-		WHERE user_name = ? `;
-		query(sql, [password, userName], (err, results, fields) => {
-			if (err) {
-				callBack(false, "数据库出错");
-				return;
-			} else {
-				callBack(true, "成功");
-				return;
-			}
-		})
-	})
-}
+// export const userChangePassword = ({ userName, password, newPassword }: UserNamePassword & { newPassword: string }, callBack: (isSuccess: boolean, msg: string) => any) => {
+// 	//修改密码，根据isSuccess参数判断是否成功
+// 	//!!!不要返回msg参数到客户端，该参数仅用于调试
+// 	userSelect({ userName, password }, (err, results, fields) => {
+// 		if (err) {
+// 			callBack(false, "数据库出错");
+// 			return;
+// 		}
+// 		if (results.length === 0) {
+// 			callBack(false, "用户不存在");
+// 			return;
+// 		}
+// 		const password = (hashPassword({ userName, password: newPassword }));
+// 		const sql = `UPDATE user_info
+// 		SET password = ?
+// 		WHERE user_name = ? `;
+// 		query(sql, [password, userName], (err, results, fields) => {
+// 			if (err) {
+// 				callBack(false, "数据库出错");
+// 				return;
+// 			} else {
+// 				callBack(true, "成功");
+// 				return;
+// 			}
+// 		})
+// 	})
+// }
 
-export const selectUserWallet = ({ userName }: { userName: string }, callBack: SqlCallBack) => {
-	// 查询用户钱包信息
-	const sql = `SELECT money,score,discounts FROM user_wallet w
-	JOIN user_info u ON w.id=u.id
-	WHERE user_name= ?`;
-	query(sql, [userName], callBack);
-}
+// export const selectUserWallet = ({ userName }: { userName: string }, callBack: SqlCallBack) => {
+// 	// 查询用户钱包信息
+// 	const sql = `SELECT money,score,discounts FROM user_wallet w
+// 	JOIN user_info u ON w.id=u.id
+// 	WHERE user_name= ?`;
+// 	query(sql, [userName], callBack);
+// }
 
-export const selectUserDiscounts = ({ id }: { id: string }, callBack: SqlCallBack) => {
-	// 根据id查看优惠券
-	const sql = `SELECT name,msg,indate FROM discounts
-	WHERE id= ? `;
-	query(sql, [id], callBack);
-}
+// export const selectUserDiscounts = ({ id }: { id: string }, callBack: SqlCallBack) => {
+// 	// 根据id查看优惠券
+// 	const sql = `SELECT name,msg,indate FROM discounts
+// 	WHERE id= ? `;
+// 	query(sql, [id], callBack);
+// }
